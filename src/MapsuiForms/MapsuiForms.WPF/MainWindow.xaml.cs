239e9365af09b9efc4f378e77fc0b74c4ed5951e
bert <btemme@hotmail.com>
2018-06-04 21:05:37 +0200

    adding wpf project

---
+++ src/MapsuiForms/MapsuiForms.WPF/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace MapsuiForms.WPF
+{
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            Forms.Init();
+            LoadApplication(new MapsuiForms.App());
+        }
+    }
+}
---
+++ src/MapsuiForms/MapsuiForms.WPF/MapViewRenderer.cs
@@ -0,0 +1,30 @@
+﻿using MapsuiForms;
+using Xamarin.Forms.Platform.WPF;
+using MapsuiForms.WPF;
+
+[assembly: ExportRenderer(typeof(MapsuiView), typeof(MapViewRenderer))]
+namespace MapsuiForms.WPF
+{
+    public class MapViewRenderer: ViewRenderer<MapsuiView, Mapsui.UI.Wpf.MapControl>
+    {
+        Mapsui.UI.Wpf.MapControl mapNativeControl;
+        MapsuiView mapViewControl;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<MapsuiView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (mapViewControl == null && e.NewElement != null)
+                mapViewControl = e.NewElement;
+
+            if (mapNativeControl == null && mapViewControl != null)
+            {
+                mapNativeControl = new Mapsui.UI.Wpf.MapControl();
+                mapNativeControl.Map = mapViewControl.NativeMap;
+
+                SetNativeControl(mapNativeControl);
+            }
+        }
+
+    }
+}
